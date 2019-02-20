518e239038bca8341a59d3da5c62a2a44a56d3fd
Tanyatorn21 <45156786+Tanyatorn21@users.noreply.github.com>
2019-02-20 15:07:41 +0700

    ค้นหาสินค้าได้แล้ว

--- src/SearchProduct/SearchProduct.js
+++ src/SearchProduct/SearchProduct.js
@@ -2,26 +2,70 @@ import React, { Component } from 'react';
 
 import {
     View,
-    Button,
+    StyleSheet,
+    Alert,
+    TouchableOpacity,
+    Text,
+    Image,
+    TouchableHighlight,
+    ListView,
+
+
+
 } from 'react-native';
 
 
-import { Input, Header } from 'react-native-elements';
+
+
+import { TextInput } from 'react-native-gesture-handler';
+import { Container, Content, Button, Icon, Picker, Form, Header, Left, Body, Title, Right } from 'native-base';
+import GLOBALS from '../GLOBALS';
+
+
+import { ProductList } from './ProductList';
+
 
 
 // const window = Dimensions.get('window');
 
 
-
+var products = [];
 export default class SearchProduct extends Component {
+
+
+    onPressButton() {
+        Alert.alert('You tapped the button!')
+    }
+
+
 
     constructor(props) {
         super(props);
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
         this.state = {
+            data_source: ds.cloneWithRows(products),
+            products: [],
             stock_group: [],
+            stock_group_id: "",
+            keyword: '',
             isLoading: false,
             Num: 0
-        }
+
+        };
+    }
+    onValueChange(value) {
+        this.setState({
+            stock_group_id: value
+        });
+
+
+        // constructor(props) {
+        //     super(props);
+        //     this.state = {
+        //         stock_group: [],
+        //         isLoading: false,
+        //         Num: 0
+        //     }
     }
 
     clickMe() {
@@ -30,13 +74,14 @@ export default class SearchProduct extends Component {
 
     componentDidMount() {
 
-        return fetch('http://192.168.0.165/RevelSoft/web_service/controller/getStockGroup.php')
+        return fetch(GLOBALS.URL + 'getStockGroup.php')
             .then((response) => response.json())
             .then((responseJson) => {
 
                 //            console.warn(responseJson.countNum)
 
                 this.setState({
+
 
                     isLoading: responseJson.result,
                     stock_group: responseJson.user,
@@ -51,38 +96,178 @@ export default class SearchProduct extends Component {
             });
 
 
+
+
     }
 
+    //-----------------------------------------------------------------------------------
+    async search() {
 
+        const Stock_group_id = this.state.stock_group_id;
+        const Keyword = this.state.keyword;
+        console.warn("stock_group_id : ", Stock_group_id);
+        console.warn("keyword : ", Keyword);
+
+        fetch(GLOBALS.URL + 'getStockReport.php', {
+
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+
+                stock_group_id: Stock_group_id,
+                keyword: Keyword,
+
+            })
+
+        })
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({
+                    data_source: this.state.data_source.cloneWithRows(responseJson.products),
+                    products: responseJson.products,
+                })
+                console.warn(responseJson.products);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+    }
+
+    //--------------------------------------------------------------------------------------------
+
+
+    renderRow(rowData) {
+        return (
+            <TouchableHighlight>
+                <View style={styles.listItem}>
+                    <View style={styles.listItemIcon}>
+                        <Icon name="rocket" color="#900" />
+                    </View>
+
+                    <View style={styles.listItemContent}>
+                        <View style={styles.listItemContentRow}>
+                            <Text style={styles.listItemContentCode}> {rowData.product_code} </Text>
+
+                        </View>
+                        <View style={styles.listItemContentRow}>
+                            <Text style={styles.listItemContentName}> {rowData.product_name} </Text>
+                        </View>
+                        <View style={styles.listItemContentRow}>
+                            <Text style={styles.listItemContentStock}>{rowData.stock_group_name} </Text>
+                            <Text style={styles.listItemContentQty}> {rowData.stock_report_qty} </Text>
+                        </View>
+                    </View>
+                </View>
+            </TouchableHighlight>
+
+
+        )
+    }
 
     render() {
-  
-       
+        //  const {navigate} = this.props.navigation;
+
+
+
+        var colNum = this.state.Num
+        var dataPicker = []
+        let data = [];
+
+        for (i = 0; i < colNum; i++) {
+            // data[i]  = {
+            dataPicker.push(
+
+                <Picker.Item label={this.state.stock_group[i].stock_group_name} value={this.state.stock_group[i].stock_group_id} />
+
+            )
+        }
+
+
+
+
         return (
             <View>
 
-                <Header
-                    leftComponent={{ icon: 'menu', color: '#fff' }}
-                    centerComponent={{ text: 'Search Product', style: { color: '#fff' } }}
-                    rightComponent={{ icon: 'home', color: '#fff' }}
-                />
+                <Header style={{ backgroundColor: '#FFFFFF' }}>
+                    <Left>
+                        <TouchableOpacity
+                            onPress={this.props.OnToggled}
+                            style={{ width: 32, height: 32 }}
+                        >
+                            <Image
+                                source={GLOBALS.image}
+                                style={{ width: 32, height: 32 }}
+                            />
+
+                        </TouchableOpacity>
+                    </Left>
+
+                    <Body>
+                        <Title>ค้นหาคลังสินค้า</Title>
+                    </Body>
+                    <Right>
+
+                    </Right>
+
+                </Header>
+
+                <Container>
+
+                    <Content>
+
+                        <Form>
+                            <Text>
+                                คลังสินค้า/Stock
+                                            </Text>
+
+                            <Picker
+                                mode="dropdown"
+                                // iosIcon={{Icon name="arrow-down" />}
+                                headerStyle={{ backgroundColor: "#b95dd3" }}
+                                headerBackButtonTextStyle={{ color: "#fff" }}
+                                headerTitleStyle={{ color: "#fff" }}
+                                selectedValue={this.state.stock_group_id}
+                                onValueChange={this.onValueChange.bind(this)}
+                            >
+
+                                <Picker.Item label='ทั้งหมด' value='0' />
+                                {dataPicker}
+                            </Picker>
+                        </Form>
 
 
-                <Input
 
-                    placeholder='คำค้น'
-                    shake={true}
-                    errorStyle={{ color: 'back' }}
-                    errorMessage='Example: T001.'
-                />
+                        <TextInput
+                            placeholder='คำค้น'
+                            shake={true}
+                            errorStyle={{ color: 'back' }}
+                            errorMessage='Example: T001.'
+                            // secureTextEntry={true}
+                            onChangeText={(pass) => this.setState({ keyword: pass })}
+                            value={this.state.keyword}
+                        />
 
-               
-                <Button
-                    onPress={onPressLearnMore}
-                    title="ค้นหา"
-                    color="#841584"
-                />
-                            
+                        <TouchableHighlight style={styles.btnSearch} onPress={() => this.search()} >
+                            <Text>ค้นหา</Text>
+                        </TouchableHighlight>
+
+                        <View style={styles.container}>
+                            <ListView
+                                style={styles.listBody}
+                                dataSource={this.state.data_source}
+                                renderRow={this.renderRow.bind(this)}
+                            />
+                        </View>
+
+                    </Content>
+                </Container>
+
+
+
 
             </View>
         )
@@ -90,7 +275,62 @@ export default class SearchProduct extends Component {
 
 
 }
-
-
-
 export { SearchProduct };
+
+const styles = StyleSheet.create({
+    btnSearch: {
+        color: 'red',
+    },
+    container: {
+        flex: 1,
+        alignItems: 'stretch',
+        justifyContent: 'center'
+    },
+    listBody: {
+        flex: 1,
+    },
+    listItem: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'stretch',
+        paddingTop: 8,
+        paddingBottom: 8
+    },
+    listItemIcon: {
+        margin: 4,
+        width: 40,
+        height: 40,
+        borderRadius: 20,
+        backgroundColor: "#efb",
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    },
+    listItemContent: {
+        flex: 1,
+        alignItems: 'stretch',
+        paddingRight: 4,
+    },
+    listItemContentRow: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'stretch'
+    },
+    listItemContentCode: {
+        flex: 1,
+        textAlign: 'left',
+    },
+    listItemContentName: {
+        flex: 1,
+        textAlign: 'left',
+    },
+    listItemContentStock: {
+        flex: 1,
+        color: "#aaa"
+    },
+    listItemContentQty: {
+        flex: 1,
+        textAlign: 'right',
+    },
+
+});
